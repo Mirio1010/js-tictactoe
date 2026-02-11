472129ae14adc86b52c6b0b0785a9f2da4f28972
Miguelangel Ortega <miguelangelortega2021@gmail.com>
2026-02-11 15:20:44 -0500

    added placing xs and os on the board

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
-console.log('hello class');
+const board = document.querySelector(".board");
+let playerTurn = 0;
+const tempArr = [
+    [,,,],
+    [,,,],
+    [,,,]
+];
+
+tempArr.forEach(item => console.log(item));
+const playerTurnHeading = document.querySelector(".playerTurn");
+board.addEventListener('click', (e) => {
+    if (playerTurn % 2 === 0) {
+        e.target.textContent = "O";
+        e.target.style.color = 'blue'
+        playerTurnHeading.textContent = `X's turn`;
+        changePlayerTurn();
+
+    } else {
+        e.target.textContent = "X";
+        e.target.style.color = "red";
+        playerTurnHeading.textContent = `O's turn`;
+        changePlayerTurn();
+    }
+    
+});
+
+
+function changePlayerTurn() {
+    playerTurn++;
+}
+
 
 
